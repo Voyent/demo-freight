d227217a693cddf7b3e28705507e9c9684797fca
Brennan McKinney <brennan.mckinney@icesoft.com>
2016-02-19 10:09:17 -0800

    Fixing notifications

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,65 +1,15 @@
 /*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
+ Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+ This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ Code distributed by Google as part of the polymer project is also
+ subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
 
 (function(document) {
   'use strict';
 
-    function setupNotificationListener(){
-      bridgeit.xio.push.attach('http://'+app.host+'/pushio/demos/realms/freight', bridgeit.io.auth.getLastKnownUsername());
-      bridgeit.xio.push.addListener(function (payload) {
-          console.log('Notification: ', payload);
-
-          //normalize payload TODO!!
-          if( payload.message && typeof payload.message === 'object' && payload.message.message){
-            payload.message = payload.message.message; 
-          }
-
-          //ignore first batch of notifications for admin as they are irrelevant
-          payload.usernameFromGroup = payload.group.split('/').pop();
-          if( payload.message === 'joined' && payload.username !== payload.usernameFromGroup ){
-            console.log('suppressing notification display');
-            return;
-          }
-
-          var messageToDisplay;
-          if( payload.message === 'joined' ){
-            messageToDisplay = payload.usernameFromGroup + ' joined';
-          }
-          else{
-            messageToDisplay = payload.message;
-          }
-
-          var demoView = app.$.demoView;
-          demoView.$$('demo-data').push('notifications', payload);
-
-          demoView.message = messageToDisplay;
-          demoView.querySelector('#toast').show();
-
-          var demoData = demoView.$$('#demoData');
-          if( demoData ){
-            if( !demoData.notificationsByUser ){
-              demoData.notificationsByUser = {};
-            }
-            var userNotificationList = demoData.notificationsByUser[payload.usernameFromGroup];
-            if( !userNotificationList ){
-              userNotificationList = demoData.notificationsByUser[payload.usernameFromGroup] = [];
-            }
-            userNotificationList.push(payload);
-            demoData.lastNotificationTimestamp = payload.time;
-          }
-          else{
-            console.warn('could not locate demoData element to store user notification');
-          }
-      });
-      window.initializePushGroups(); //delegates to index.html for admins or client.html for regular users
-    }
-    
   function finishLazyLoading(){
     // Grab a reference to our auto-binding template
     // and give it some initial binding values
@@ -86,7 +36,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
           //initialize lastNotificationTimestamp so user list displays
           var demoData = app.$.demoView.$$('#demoData');
           if( demoData ){
-             demoData.lastNotificationTimestamp = new Date().getTime();
+            demoData.lastNotificationTimestamp = new Date().getTime();
           }
         }, 5000);
       }
@@ -97,6 +47,58 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
       console.log('onAfterLogin callback: configuring notifications');
       setupNotificationListener();
     });
+
+    function setupNotificationListener(){
+      bridgeit.xio.push.attach('http://'+app.host+'/pushio/demos/realms/freight', bridgeit.io.auth.getLastKnownUsername());
+      bridgeit.xio.push.addListener(function (payload) {
+        console.log('Notification: ', payload);
+
+        //normalize payload TODO!!
+        if( payload.message && typeof payload.message === 'object' && payload.message.message){
+          payload.message = payload.message.message;
+        }
+
+        //ignore first batch of notifications for admin as they are irrelevant
+        payload.usernameFromGroup = payload.group.split('/').pop();
+        if( payload.message === 'joined' && payload.username !== payload.usernameFromGroup ){
+          console.log('suppressing notification display');
+          return;
+        }
+
+        var messageToDisplay;
+        if( payload.message === 'joined' ){
+          messageToDisplay = payload.usernameFromGroup + ' joined';
+        }
+        else{
+          messageToDisplay = payload.message;
+        }
+
+        var demoView = app.$.demoView;
+        demoView.$$('demo-data').push('notifications', payload);
+
+        demoView.message = messageToDisplay;
+        demoView.querySelector('#toast').show();
+
+        var demoData = demoView.$$('#demoData');
+        if( demoData ){
+          if( !demoData.notificationsByUser ){
+            demoData.notificationsByUser = {};
+          }
+          var userNotificationList = demoData.notificationsByUser[payload.usernameFromGroup];
+          if( !userNotificationList ){
+            userNotificationList = demoData.notificationsByUser[payload.usernameFromGroup] = [];
+          }
+          userNotificationList.push(payload);
+          demoData.lastNotificationTimestamp = payload.time;
+        }
+        else{
+          console.warn('could not locate demoData element to store user notification');
+        }
+      });
+      window.initializePushGroups(); //delegates to index.html for admins or client.html for regular users
+    }
+
+
 
     // See https://github.com/Polymer/polymer/issues/1381
     window.addEventListener('WebComponentsReady', function() {
@@ -112,6 +114,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
       console.log('demo app received event bridgeit-session-expired', e);
       bridgeit.xio.push.disconnect();
     });
+
+
 
     // Main area's paper-scroll-header-panel custom condensing transformation of
     // the appName in the middle-container and the bottom title in the bottom-container.
@@ -153,9 +157,9 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   }
 
   var webComponentsSupported = (
-    'registerElement' in document &&
-    'import' in document.createElement('link') &&
-    'content' in document.createElement('template'));
+  'registerElement' in document &&
+  'import' in document.createElement('link') &&
+  'content' in document.createElement('template'));
 
   if( !webComponentsSupported ){
     var script = document.createElement('script');
