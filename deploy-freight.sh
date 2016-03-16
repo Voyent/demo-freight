c5b6c03f9c3d1f8f07e1251642ec6e783de5e83b
philipbreau <philip.breau@icesoft.com>
2016-03-16 12:52:17 -0300

    upgrade to psk 1.3.0

--- deploy-freight.sh
+++ deploy-freight.sh
@@ -5,7 +5,7 @@ HOST=web1d
 echo "...Updating from Git..."
 git pull
 echo "...Removing old Bower components and Dist..."
-rm -rf ./bower_components
+rm -rf ./app/bower_components
 rm -rf ./dist
 echo "...Updating Bower with new install..."
 bower install
