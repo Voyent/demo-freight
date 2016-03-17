4bda091edd074ab745b1248408ac6d78f81fc279
philipbreau <philip.breau@icesoft.com>
2016-03-17 12:47:51 -0300

    add environment arg handling to deploy script

--- deploy-freight.sh
+++ deploy-freight.sh
@@ -2,6 +2,8 @@
 set +e
 HOST=web1d
 
+ENVIRONMENT=$@
+
 echo "...Updating from Git..."
 git pull
 echo "...Removing old Bower components and Dist..."
@@ -17,7 +19,16 @@ tar -zcf ../freight.tar.gz .
 cd ..
 echo "...Copying Dist to server..."
 scp -i ~/.ssh/ICEsoft_Linux_Test_Key_Pair.pem freight.tar.gz ubuntu@web1d:~/. || { exit 1; }
+
+if [[ $ENVIRONMENT == *'staging'* ]] ; then
+	DESTDIR="/usr/share/nginx/html/static/demos/staging/freight"
+else
+	DESTDIR="/usr/share/nginx/html/static/demos/freight"
+fi
+
+echo "Remote destination directory = $DESTDIR"
+
 echo "...Unpacking Dist on server..."
-ssh -i ~/.ssh/ICEsoft_Linux_Test_Key_Pair.pem ubuntu@web1d "sudo tar -zxf /home/ubuntu/freight.tar.gz -C /usr/share/nginx/html/static/demos/freight"
+ssh -i ~/.ssh/ICEsoft_Linux_Test_Key_Pair.pem ubuntu@web1d "sudo tar -zxf /home/ubuntu/freight.tar.gz -C $DESTDIR"
 echo "...Cleaning up local compressed file..."
 rm freight.tar.gz
